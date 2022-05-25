1771bef7a9d385cfffdff484d576272b45c4bba2
Nikolaos Tsoukas <Nick-tsoukas>
2022-05-25 11:43:21 -0500

    chore: fetching data

--- pages/index.vue
+++ pages/index.vue
@@ -4,30 +4,44 @@
     <section>
       <h1 class="text-center my-10">Featured Bands</h1>
       <div>
-        <Featbands :bands="feats" />
+        <!-- <Featbands :bands="feats" /> -->
       </div>
     </section>
     <section>
       <h1 class="text-center my-10">All bands</h1>
-      <BandCard :bands="bands" />
+      <h2>There may be the band array here</h2>
+      <pre>{{ bands }}</pre>
+      <!-- <BandCard :bands="bands" /> -->
     </section>
   </div>
 </template>
 
 <script>
 export default {
-  async asyncData({ $strapi }) {
+  // async asyncData({ $strapi }) {
+  //   try {
+  //     const bands = await $strapi.find('bands')
+  //     const event = await $strapi.find('featured-event')
+  //     const videos = await $strapi.find('videos')
+  //     const feats = bands.slice(0, 3)
+  //     return {
+  //       bands,
+  //       feats,
+  //       event,
+  //       videos,
+  //     }
+  //   } catch (error) {
+  //     return error
+  //   }
+  // },
+  data() {
+    return {
+      bands: [] || {},
+    }
+  },
+  async mounted() {
     try {
-      const bands = await $strapi.find('bands')
-      const event = await $strapi.find('featured-event')
-      const videos = await $strapi.find('videos')
-      const feats = bands.slice(0, 3)
-      return {
-        bands,
-        feats,
-        event,
-        videos,
-      }
+      this.bands = await this.$strapi.find('bands')
     } catch (error) {
       return error
     }
