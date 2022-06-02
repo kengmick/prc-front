de768868bb9537b9374762cb03b07a773513335a
Nikolaos Tsoukas <Nick-tsoukas>
2022-06-02 12:22:54 -0500

    chore: adding classified and comments

--- pages/classifieds.vue
+++ pages/classifieds.vue
@@ -1,9 +1,59 @@
 <template>
   <div>
-    <h1>Classified Page</h1>
+    <h1 class="chedder main_red_text text-center mb-6">Classified Page</h1>
+    <section
+      v-if="classifieds"
+      class="container mx-auto px-4 flex flex-col items-start"
+    >
+      <div v-for="(article, index) in classifieds" :key="index" class="my-6">
+        <nuxtLink
+          :to="{ path: 'classifiedview', query: { classified: article.id } }"
+          class="chedder text-2xl underline underline-offset-2 text-blue-700 hover:text-red-700"
+        >
+          <span class="text-black pr-6">{{
+            moment(String(article.created_at)).format('MMM Do YY')
+          }}</span>
+          {{ article.title }}
+          <span class="text-black pl-6"
+            >({{ article.city }}, {{ article.state }})</span
+          >
+          <span class="text-black pl-6 ptmono"
+            >Category {{ article.category }}
+          </span>
+        </nuxtLink>
+      </div>
+    </section>
+    <!-- <pre>{{ classifieds }}</pre> -->
   </div>
 </template>
 
 <script>
-export default {}
+import moment from 'moment'
+
+export default {
+  data() {
+    return {
+      classifieds: [],
+    }
+  },
+  async mounted() {
+    const classifieds = await this.$strapi.find('classifieds')
+    this.classifieds = classifieds.sort(function (a, b) {
+      return moment(b.created_at).format('X') - moment(a.created_at).format('X')
+    })
+  },
+  methods: {
+    moment,
+  },
+}
 </script>
+
+<style scoped>
+.btn_custom {
+  padding: 0.5em 1em;
+  border: 1px solid black;
+  background: black;
+  color: white;
+  cursor: pointer;
+}
+</style>
--- pages/classifiedview.vue
+++ pages/classifiedview.vue
@@ -1,9 +1,63 @@
 <template>
   <div>
-    <h1>This is the single classifed view of a article</h1>
+    <section v-if="classified">
+      <div class="container mx-auto my-10">
+        <p class="pb-4">
+          Posted on :
+          {{ moment(String(classified.created_at)).format('MMM Do YY') }}
+        </p>
+        <h2 class="text-2xl">{{ classified.title }}</h2>
+        <div v-if="classified.description.split('\n')" class="pb-6">
+          <p
+            v-for="(description, index) in classified.description.split('\n')"
+            :key="description + index"
+            class="my-4"
+          >
+            {{ description }}
+          </p>
+        </div>
+        <p v-else>
+          {{ classified.description }}
+        </p>
+        <p class="pb-1">Contact Info: {{ classified.contact }}</p>
+        <p v-if="classified.phone" class="pb-1">
+          Cell Phone {{ classified.phone }}
+        </p>
+        <!-- <p v-if="article.phone" class="pb-1">Phone: {{ classified.phone }}</p> -->
+        <p>Username : {{ classified.users_permissions_user.username }}</p>
+      </div>
+    </section>
+    <section class="container mx-auto my-10">
+      <h2 class="text-center text-2xl">Comments</h2>
+      <div class="container shadow-2xl size p-10 my-10">fdsfadsfasdfadsf</div>
+    </section>
   </div>
 </template>
 
 <script>
-export default {}
+import moment from 'moment'
+export default {
+  data() {
+    return {
+      classified: '',
+    }
+  },
+  async mounted() {
+    const classified = await this.$strapi.findOne(
+      'classifieds',
+      this.$route.query.classified
+    )
+    this.classified = classified
+  },
+
+  methods: {
+    moment,
+  },
+}
 </script>
+
+<style scoped>
+.size {
+  min-height: 200px;
+}
+</style>
