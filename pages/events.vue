5989b1d6b86ee818f87bcc8af4cc1e2b52d723b6
Nikolaos Tsoukas <Nick-tsoukas>
2022-06-02 14:34:49 -0500

    chore: ...

--- pages/events.vue
+++ pages/events.vue
@@ -1,11 +1,32 @@
 <template>
-  <div>
-    <h1 class="text-5xl text-center main_red_text">This is the events page</h1>
+  <div v-if="events">
+    <h1 class="text-5xl text-center main_red_text">Events</h1>
+    <section>
+      <div v-for="(event, index) in events" :key="events.title + index">
+        <p>{{ event.title }}</p>
+      </div>
+    </section>
+    <pre>{{ events }}</pre>
   </div>
 </template>
 
 <script>
-export default {}
+import moment from 'moment'
+export default {
+  data() {
+    return {
+      events: '',
+    }
+  },
+  async mounted() {
+    const events = await this.$strapi.find('events')
+    this.events = events
+  },
+
+  methods: {
+    moment,
+  },
+}
 </script>
 
 <style lang="scss" scoped></style>
