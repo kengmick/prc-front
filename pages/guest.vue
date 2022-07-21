ea305c3b287e77f2ab84759f3c7ff0ce2677f22d
Nikolaos Tsoukas <Nick-tsoukas>
2022-07-21 16:41:35 -0500

    chore: add membership card

--- pages/guest.vue
+++ pages/guest.vue
@@ -2,9 +2,29 @@
   <div class="container mx-auto flex justify-center flex-col px-2 py-2">
     <h1>Free Membership and become a Guest</h1>
     <section>
-      <FormulateForm :values="formValues">
-        <FormulateInput type="text" name="name" label="Your name" />
-        <FormulateInput type="email" name="email" label="Your email" />
+      <FormulateForm v-model="formValues" @submit="submitForm">
+        <FormulateInput
+          name="username"
+          label="Username"
+          validation="required"
+          wrapper-class="m-auto sm:w-4/5 "
+          element-class="w-full"
+          errors-class="sm:w-4/5 m-auto"
+        />
+        <FormulateInput
+          type="email"
+          name="email"
+          label="Your email"
+          validation="required"
+        />
+        <FormulateInput
+          name="password"
+          label="Create a password"
+          validation="required"
+          wrapper-class="m-auto sm:w-4/5 "
+          element-class="w-full"
+          errors-class="sm:w-4/5 m-auto"
+        />
         <FormulateInput type="submit" label="Save account" />
       </FormulateForm>
     </section>
@@ -12,7 +32,31 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      formValues: {},
+    }
+  },
+  methods: {
+    async submitForm() {
+      try {
+        console.log(this.formValues)
+        const user = await this.$strapi.register({
+          email: this.formValues.email,
+          username: this.formValues.username,
+          password: this.formValues.password,
+          acc: 1,
+        })
+        if (user) {
+          this.$router.push({ path: 'membercard', query: { user: user.id } })
+        }
+      } catch (error) {
+        this.errorMessage = error
+      }
+    },
+  },
+}
 </script>
 
 <style lang="scss" scoped></style>
---
+++ pages/membercard.vue
@@ -0,0 +1,14 @@
+<template>
+  <div
+    v-if="$strapi.user"
+    class="w-screen h-screen flex justify-center items-center bg-green-500"
+  >
+    <div class="bg-black text-white">Membership!!!</div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
