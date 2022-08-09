8d302a45226812a551fa05aa00231a9a1d63dda6
Nikolaos Tsoukas <Nick-tsoukas>
2022-08-09 18:21:29 -0500

    chrore: admin page

--- pages/admin.vue
+++ pages/admin.vue
@@ -2,16 +2,50 @@
   <div>
     <section v-if="superman">
       <h1 class="text-center text-main-red">Admin</h1>
-      <p>Admin page</p>
-      <FormulateForm v-model="formValues" @submit="submitForm">
-        <FormulateInput
-          type="text"
-          label="Video Id"
-          wrapper-class="w-full mt-10 px-4 sm:mx-10"
-          grouping-class="bg-black"
-          element-class="w-full"
-        />
-      </FormulateForm>
+
+      <section class="flex justify-center items-center w-full my-10">
+        <FormulateForm v-model="formValues" @submit="submit">
+          <FormulateInput
+            name="live"
+            type="text"
+            outer="w-full"
+            label="Live stream Id"
+            placeholder="Sample placeholder"
+            help="Put the id of live stream here"
+            input-class="w-full sm:w-96 "
+            wrapper-class="w-full sm:w-96 "
+            element-class="w-full sm:w-96 "
+            error-behavior="live"
+          />
+          <FormulateInput
+            type="submit"
+            label="Create Live Stream"
+            wrapper-class="flex justify-center mt-6"
+          />
+        </FormulateForm>
+        <br />
+        <pre>{{ liveStream }}</pre>
+      </section>
+    </section>
+    <section
+      v-if="loading"
+      class="h-screen w-screen fixed right-0 flex justify-center items-center top-0 bg-white z-50"
+    >
+      <Spinner />
+    </section>
+    <section
+      v-if="errorMessage"
+      class="h-screen w-screen fixed right-0 flex justify-center items-center top-0 bg-white z-50"
+    >
+      <div>
+        <h2>{{ errorMessage }}</h2>
+        <h3
+          class="text-center text-2xl cursor-pointer"
+          @click="errorMessage = null"
+        >
+          Close X
+        </h3>
+      </div>
     </section>
   </div>
 </template>
@@ -21,10 +55,15 @@ export default {
   data() {
     return {
       superman: false,
+      formValues: {},
+      loading: false,
+      errorMessage: '',
+      liveStream: 'default',
     }
   },
-  mounted() {
+  async mounted() {
     if (this.$strapi.user) {
+      this.liveStream = (await this.$strapi.find('live-stream')).streamLink
       if (this.$strapi.user.username === 'superman') {
         this.superman = true
       } else {
@@ -32,7 +71,21 @@ export default {
       }
     }
   },
+  methods: {
+    async submit() {
+      this.loading = true
+
+      try {
+        const liveStream = await this.$strapi.update('live-stream', {
+          streamLink: this.formValues.live,
+        })
+        this.liveStream = liveStream.streamLink
+        this.loading = false
+      } catch (error) {
+        this.loading = false
+        this.errorMessage = 'Sorry there was an error'
+      }
+    },
+  },
 }
 </script>
-
-<style lang="scss" scoped></style>
