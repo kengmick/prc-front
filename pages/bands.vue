869f23bddb1461337e49d821cab7d8ca7cfa575a
Nikolaos Tsoukas <Nick-tsoukas>
2022-11-06 16:36:44 -0600

    chore: testing

--- pages/bands.vue
+++ pages/bands.vue
@@ -68,7 +68,7 @@ export default {
     return {
       searchClient: instantMeiliSearch(
         'http://143.110.230.105',
-        'OTRmM2M3MGE3NGJlN2FlMGIxYWMwN2E2'
+        {code: 'OTRmM2M3MGE3NGJlN2FlMGIxYWMwN2E2'}
       ),
       bands: [],
       errorMessage: '',
