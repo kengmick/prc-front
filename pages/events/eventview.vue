e260fe6f27e70daf50456e3a645a51e98075e9fd
Nikolaos Tsoukas <Nick-tsoukas>
2023-02-03 20:25:22 -0600

    chore: ...

--- pages/events/eventview.vue
+++ pages/events/eventview.vue
@@ -5,6 +5,7 @@
         class="mx-auto"
         :event="event"
         @startChat="startChatNow(event.users_permissions_user)"
+        @updatedFavs="updatedFavs('events', event.id)"
         :isFav="favCheck('events', event.id)"
       />
     </section>
@@ -378,18 +379,40 @@ export default {
   },
   methods: {
     moment,
-    // favCheck(type, id) {
-    //   const check = this.favs.filter((f) => {
-    //     console.log('fav checkc ')
-    //     return f.data.id === id
-    //   })
-    //   if (check.length > 0) {
-    //     return true
-    //   }
-    //   console.log(check, ' this is check ')
-    // },
+
+    async updatedFavs(type, id) {
+      console.log(
+        'this is the updated favs event emited from the parent component'
+      )
+      if (this.$strapi.user) {
+        const f = await this.$strapi.find('favs', {
+          users_permissions_user: this.$strapi.user.id,
+        })
+        this.favs = f.sort((a, b) => {
+          if (a.type < b.type) {
+            return -1
+          }
+          if (a.type > b.type) {
+            return 1
+          }
+          return 0
+        })
+
+        if (this.favs !== null) {
+          const check = this.favs.filter((f) => {
+            console.log('fav checkc ')
+            return f.data.id === id
+          })
+          if (check.length > 0) {
+            return true
+          }
+          console.log(check, ' this is check ')
+        }
+      }
+    },
     favCheck(type, id) {
-      if (this.favs) {
+      console.log('emited from the child component')
+      if (this.favs !== null) {
         const check = this.favs.filter((f) => {
           console.log('fav checkc ')
           return f.data.id === id
