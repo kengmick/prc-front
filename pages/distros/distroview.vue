420a94790e232292006f7ccbea90c8ea6c370e1e
Nikolaos Tsoukas <Nick-tsoukas>
2023-02-03 20:01:55 -0600

    chore: fixing full card favorite function

--- pages/distros/distroview.vue
+++ pages/distros/distroview.vue
@@ -5,6 +5,7 @@
         :isFav="favCheck('record-labels', distro.id)"
         class="mx-auto"
         :distro="distro"
+        @updatedFavs="updatedFavs('record-labels', distro.id)"
         @startChat="startChatNow(distro.users_permissions_user)"
       />
     </section>
@@ -404,8 +405,39 @@ export default {
     })
   },
   methods: {
+    async updatedFavs(type, id) {
+      console.log(
+        'this is the updated favs event emited from the parent component'
+      )
+      if (this.$strapi.user) {
+        const f = await this.$strapi.find('favs', {
+          users_permissions_user: this.$strapi.user.id,
+        })
+        this.favs = f.sort((a, b) => {
+          if (a.type < b.type) {
+            return -1
+          }
+          if (a.type > b.type) {
+            return 1
+          }
+          return 0
+        })
+
+        if (this.favs !== null) {
+          const check = this.favs.filter((f) => {
+            console.log('fav checkc ')
+            return f.data.id === id
+          })
+          if (check.length > 0) {
+            return true
+          }
+          console.log(check, ' this is check ')
+        }
+      }
+    },
     favCheck(type, id) {
-      if (this.favs) {
+      console.log('emited from the child component')
+      if (this.favs !== null) {
         const check = this.favs.filter((f) => {
           console.log('fav checkc ')
           return f.data.id === id
