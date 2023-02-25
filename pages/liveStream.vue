d50e20bf4cf8197e7d006b958a350fc9dff6398a
Nikolaos Tsoukas <Nick-tsoukas>
2023-02-25 09:54:19 -0600

    chore: changing video page width

--- pages/liveStream.vue
+++ pages/liveStream.vue
@@ -1,11 +1,8 @@
 <template>
   <section>
-    <h1 class="main_page_padding_left_right text-2xl text-center pt-6">
-      24/7 Video Stream id:
-    </h1>
     <div class="flex justify-center mb-10">
       <div v-if="videoFile">
-        <video width="320" height="240" controls>
+        <video controls>
           <source :src="videoFile" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
