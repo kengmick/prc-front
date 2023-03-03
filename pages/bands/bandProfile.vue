1051a34c91febdf3f3c1dd21395055ab14aa9910
Nikolaos Tsoukas <Nick-tsoukas>
2023-03-03 16:06:54 -0600

    chore: adding script tag to config

--- pages/bands/bandProfile.vue
+++ pages/bands/bandProfile.vue
@@ -1,5 +1,27 @@
 <template>
   <div v-if="band" class="absolute top-[48px] h-auto">
+    <script>
+      window.fbAsyncInit = function () {
+        FB.init({
+          appId: '580986866833242',
+          xfbml: true,
+          version: 'v16.0',
+        })
+        FB.AppEvents.logPageView()
+      }
+
+      ;(function (d, s, id) {
+        var js,
+          fjs = d.getElementsByTagName(s)[0]
+        if (d.getElementById(id)) {
+          return
+        }
+        js = d.createElement(s)
+        js.id = id
+        js.src = 'https://connect.facebook.net/en_US/sdk.js'
+        fjs.parentNode.insertBefore(js, fjs)
+      })(document, 'script', 'facebook-jssdk')
+    </script>
     <MobileBand
       :band="band"
       :user="band.users_permissions_user"
