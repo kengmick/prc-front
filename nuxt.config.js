d868630ed1adb945a7636d54d8f469bbf2dfa326
Nikolaos Tsoukas <Nick-tsoukas>
2023-03-07 12:18:27 -0600

    chore: ...

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,10 +2,10 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   loading: '~/components/Loading.vue',
   target: 'server',
+  ssr: true,
   generate: {
     fallback: true,
   },
-  ssr: true,
   server: {
     port: process.env.NODE_ENV === 'development' ? 3000 : 8080,
     host: process.env.NODE_ENV === 'development' ? 'localhost' : '0.0.0.0',
