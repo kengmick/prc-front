c03b8867dbe50c73c03a4763ec855f5cd658cf4c
Nikolaos Tsoukas <Nick-tsoukas>
2023-03-07 19:11:22 -0600

    chore: ....

--- server-middleware/social.js
+++ server-middleware/social.js
@@ -1,18 +1,22 @@
 const app = require('express')()
 app.get('/getJSON', (req, res) => {
-  const temp = `<!DOCTYPE html>
-    <html lang="en">
-      <head>
-      <meta property="og:url" content="https://punkrockcompound.com/bands/86">
-      <meta property="og:type" content="website">
-      <meta property="og:title" content="this is my new meta tag">
-      <meta property="og:description" content="my best description ever ">
-      <meta property="og:image" content="https://punkrockcompund.s3.amazonaws.com/akshay_chauhan_1_Sgz_Vj4_Pfa4_unsplash_cc8e9b1203.jpg">
-      </head>
-      <body>
-      </body>
-    </html>`
-  res.send(temp)
+  fetch(`https://punkrockcompound-backend-lb57o.ondigitalocean.app/bands/86`)
+    .then((response) => response.json())
+    .then((data) => {
+      const temp = `<!DOCTYPE html>
+        <html lang="en">
+          <head>
+          <meta property="og:url" content="https://punkrockcompound.com/bands/86">
+          <meta property="og:type" content="website">
+          <meta property="og:title" content="this is my new meta tag">
+          <meta property="og:description" content="my best description ever ">
+          <meta property="og:image" content="${data.bandProfileImg.url}">
+          </head>
+          <body>
+          </body>
+        </html>`
+      res.send(temp)
+    })
 })
 
 module.exports = app
